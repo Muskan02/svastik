0e447c69aaecdbac0706ebb1388e76a09aa53453
= <=>
2020-09-15 17:31:00 +0530

    contact form working

---
+++ js/contactform.js
@@ -0,0 +1,129 @@
+jQuery(document).ready(function ($) {
+  "use strict";
+
+  //Contact
+  $('form.contact-form').submit(function () {
+    var f = $(this).find('.form-group'),
+      ferror = false,
+      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+
+    f.children('input').each(function () { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'email':
+            if (!emailExp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'checked':
+            if (!i.is(':checked')) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'regexp':
+            exp = new RegExp(exp);
+            if (!exp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    f.children('textarea').each(function () { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    if (ferror) return false;
+    else var str = $(this).serialize();
+    sendmail();
+    $("#sendmessage").show();
+    $("#errormessage").removeClass("show");
+    $('.contact-form').find("input, textarea").val("");
+    return true;
+  });
+
+});
+
+async function getData(url) {
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'no-cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  });
+  return "done";
+}
+
+
+function sendmail() {
+  Name = document.getElementById("name").value;
+  Email = document.getElementById("email").value;
+  subject = document.getElementById("subject").value;
+  message = document.getElementById("message").value;
+  body = "<h4>Name:" + Name + "<br>Email: " + Email + "<br>Subject: " + subject + "<br>Message: " + message + "</h4>";
+  var url = 'https://smartmenu.pythonanywhere.com/mail?from=noreply.jumblejuggle@gmail.com&to=info.svastik@gmail.com&subject=Contact from https://svastik.co.in&body=You got some message&html=' + body
+  getData(url);
+  reply = "<h4> Hii " + Name + ", <br><br>Hope you are well. <br><br>We have received your query subjected - <b>" + subject + "</b>.<br><br>We will contact to you shortly with more details. <br><br>Regards,</h6><br><h3>Team Svastik</h3>";
+  var url = 'https://smartmenu.pythonanywhere.com/mail?from=noreply.jumblejuggle@gmail.com&to=' + Email + '&subject=Contact from https://svastik.co.in&body=You got some message&html=' + reply
+  getData(url);
+
+}
